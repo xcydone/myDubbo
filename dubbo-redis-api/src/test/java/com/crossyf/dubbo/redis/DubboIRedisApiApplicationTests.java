55da9d1076bfdae989bee931a252b26b06597325
xcydone <1641265515@qq.com>
2020-07-29 14:54:13 +0800

    提交删除的文件

---
+++ dubbo-redis-api/src/test/java/com/crossyf/dubbo/redis/DubboIRedisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossyf.dubbo.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboIRedisApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
